5c3d0bc1c8a3d25c36809cc91fc577b551120923
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-28 21:09:53 +0530

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Dashboard, Edit, Error, Home, Register } from './pages';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Main App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
