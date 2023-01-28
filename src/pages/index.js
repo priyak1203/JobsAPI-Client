4f3a35d9531de65bd56000a73027d627f1e13d89
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-28 20:50:08 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Dashboard from './Dashboard';
+import Edit from './Edit';
+import Error from './Error';
+import Home from './Home';
+import PrivateRoute from './PrivateRoute';
+import Register from './Register';
+
+export { Dashboard, Edit, Error, Home, PrivateRoute, Register };
