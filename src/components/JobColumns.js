ee698ba490f849ff13bb4858f9f84617a1f30bb9
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-31 00:40:39 +0530

    jobs component ui in dashboard page

---
+++ src/components/JobColumns.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+function JobColumns() {
+  return (
+    <Wrapper>
+      <span>position</span>
+      <span>company</span>
+      <span>date</span>
+      <span>status</span>
+      <span className="action">action</span>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: none;
+  @media (min-width: 992px) {
+    display: grid;
+    background: var(--grey-200);
+    color: var(--grey-500);
+    border-top-left-radius: var(--borderRadius);
+    border-top-right-radius: var(--borderRadius);
+    grid-template-columns: 1fr 1fr 150px 100px 100px;
+    align-items: center;
+    padding: 1rem 1.5rem;
+    column-gap: 1rem;
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    font-size: var(--small-text);
+    font-weight: 600;
+    .action {
+      margin-left: 1rem;
+    }
+  }
+`;
+
+export default JobColumns;
